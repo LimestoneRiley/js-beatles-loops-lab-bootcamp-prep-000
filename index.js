15f6c6f4d799095606f40bd31f469872892ea7e3
Robert Riley <rob_a_riley@yahoo.com>
2018-09-26 17:41:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments){
   var BeatlesArray = [];
-  
+  var i;
   for (i = 0; i < musicians.length; i++){
     BeatlesArray[i] = musicians[i] + " plays " + instruments[i];
   }
