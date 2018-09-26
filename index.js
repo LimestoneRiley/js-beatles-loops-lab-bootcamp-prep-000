35c85ebaeeef812f7b8580611da36424aff9de0b
Robert Riley <rob_a_riley@yahoo.com>
2018-09-26 17:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (musicians, instruments){
   var BeatlesArray = [];
   
   for ( i = 0; i < musicians.length; i++){
-    
+    BeatlesArray.pop 
   }
   
   return (BeatlesArray);
