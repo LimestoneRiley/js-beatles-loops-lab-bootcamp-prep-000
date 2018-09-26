b4f669250ce9b11e50098a86d925281c84a71861
Robert Riley <rob_a_riley@yahoo.com>
2018-09-26 17:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay (musicians, instruments){
 function johnLennonFacts (facts){
   var i = 0;
   while (i < facts.length){
-    facts[i] = fact[i] + "!!!";
+    facts[i] = facts[i] + "!!!";
   }
   return (facts);
 }
